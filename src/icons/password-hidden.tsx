4c441f4f9ea949f70f9697cdf05ba185c43b70e1
Mykhailo Fialko <Mykhailo_Fialko@epam.com>
2024-05-20 18:49:43 +0300

    props types adjustments

--- src/icons/password-hidden.tsx
+++ src/icons/password-hidden.tsx
@@ -1,8 +1,9 @@
 
 
+interface IconProps extends React.SVGProps<SVGSVGElement> {}
 
 
-const PasswordHiddenIcon = ({ ...props }: any) => {
+const PasswordHiddenIcon = (props: IconProps) => {
 
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
--- src/icons/password-shown.tsx
+++ src/icons/password-shown.tsx
@@ -1,6 +1,6 @@
+interface IconProps extends React.SVGProps<SVGSVGElement> {}
 
-
-const PasswordShownIcon = ({ ...props }: any) => {
+const PasswordShownIcon = (props: IconProps) => {
 
     return (
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
